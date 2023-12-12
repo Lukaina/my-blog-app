750151543560845c5205c114fa5cf1a74866c53a
Verónica Alcaraz <veronica.alcaraz@mercadolibre.com.co>
2023-12-11 21:27:47 -0500

    feat(service): add users service

---
+++ src/users/services/users.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
+import { User } from '../models/user.model';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = new this.userModel(createUserDto);
+    return await newUser.save();
+  }
+
+  async findById(id: string): Promise<User> {
+    return await this.userModel.findById(id).exec();
+  }
+
+  async update(id: string, updateUserDto: CreateUserDto): Promise<User> {
+    return await this.userModel
+      .findByIdAndUpdate(id, updateUserDto, { new: true })
+      .exec();
+  }
+
+  async updateUser(user: User, updateUserDto: UpdateUserDto): Promise<User> {
+    if (updateUserDto.name) {
+      user.name = updateUserDto.name;
+    }
+
+    if (updateUserDto.email) {
+      user.email = updateUserDto.email;
+    }
+    return user;
+  }
+
+  async remove(id: string): Promise<void> {
+    const result = await this.userModel.deleteOne({ _id: id }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('User not found');
+    }
+  }
+}
