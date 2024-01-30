e3e5036602324cfdfd9d712c1844365b33887ba7
Verónica Alcaraz <veronica.alcaraz@mercadolibre.com.co>
2024-01-30 08:48:34 -0500

    feat: correction url mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/projectVero')],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/blog'),
+  ],
   controllers: [],
   providers: [],
 })
